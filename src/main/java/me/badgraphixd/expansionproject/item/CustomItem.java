aa8169e644c8f584e9e1f6b0e88e75d187264b91
BadGraphixD <badgraphixcontact@gmail.com>
2022-09-13 19:32:15 +0200

    Spell system implemented and tested, Database system tested - 2nd commit (files were missing)

--- src/main/java/me/badgraphixd/expansionproject/item/CustomItem.java
+++ src/main/java/me/badgraphixd/expansionproject/item/CustomItem.java
@@ -48,7 +48,7 @@ public class CustomItem {
     }
 
     public ItemStack getItem() {
-        return item;
+        return item.clone();
     }
 
 }
--- src/main/java/me/badgraphixd/expansionproject/magic/EffectInvocation.java
+++ src/main/java/me/badgraphixd/expansionproject/magic/EffectInvocation.java
@@ -1,12 +1,17 @@
 package me.badgraphixd.expansionproject.magic;
 
 import org.bukkit.Location;
-
-import javax.swing.text.html.parser.Entity;
+import org.bukkit.entity.Entity;
 
 public class EffectInvocation {
 
     public Entity invoker;
     public Location location;
+    public Entity involved;
 
+    public EffectInvocation(Entity invoker, Location location, Entity involved) {
+        this.invoker = invoker;
+        this.location = location;
+        this.involved = involved;
+    }
 }
